dc79d94af3d054851555384f93f403a51be70971
wid144-afk <wid144@gmail.com>
2026-02-13 13:41:00 +0000

    ch2 l6

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,7 @@
+from platform import node
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -23,6 +24,50 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.BOLD, url="https://boot.dev")
         node2 = TextNode("This is a text node", TextType.BOLD, url="https://boot.dev")
         self.assertEqual(node, node2)
+    
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_bold(self):
+        node = TextNode("This is a bold text node", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is a bold text node")
+    
+    def test_italic(self):
+        node = TextNode("This is an italic text node", TextType.ITALIC)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "i")
+        self.assertEqual(html_node.value, "This is an italic text node")
+
+    def test_code(self):
+        node = TextNode("This is a code text node", TextType.CODE)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "code")
+        self.assertEqual(html_node.value, "This is a code text node")
+    
+    def test_link(self):
+        node = TextNode("This is a link text node", TextType.LINK, url="https://boot.dev")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "This is a link text node")
+        self.assertEqual(html_node.props["href"], "https://boot.dev")
+
+    def test_image(self):
+        node = TextNode("This is an image text node", TextType.IMAGE, url="https://boot.dev/image.png")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(html_node.props["src"], "https://boot.dev/image.png")
+        self.assertEqual(html_node.props["alt"], "This is an image text node")
+
+    def test_invalid(self):
+        node = TextNode("This is an invalid text node", "invalid")
+        with self.assertRaises(ValueError):
+            text_node_to_html_node(node)
 
 
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -1,7 +1,8 @@
 from enum import Enum
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 class TextType(Enum):
-    PLAIN = "plain"
+    TEXT = "text"
     BOLD = "bold"
     ITALIC = "italic"
     CODE = "code"
@@ -21,3 +22,23 @@ class TextNode:
     
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+    
+def text_node_to_html_node(text_node):
+    if text_node.text_type == TextType.TEXT:
+        return LeafNode(tag=None, value=text_node.text)
+    elif text_node.text_type == TextType.BOLD:
+        return LeafNode(tag="b", value=text_node.text)
+    elif text_node.text_type == TextType.ITALIC:
+        return LeafNode(tag="i", value=text_node.text)
+    elif text_node.text_type == TextType.CODE:
+        return LeafNode(tag="code", value=text_node.text)
+    elif text_node.text_type == TextType.LINK:
+        if text_node.url is None:
+            raise ValueError("Link text nodes must have a url")
+        return LeafNode(tag="a", value=text_node.text, props={"href": text_node.url})
+    elif text_node.text_type == TextType.IMAGE:
+        if text_node.url is None:
+            raise ValueError("Image text nodes must have a url")
+        return LeafNode(tag="img", value="", props={"src": text_node.url, "alt": text_node.text})
+    else:
+        raise ValueError(f"Unsupported text type: {text_node.text_type}")
